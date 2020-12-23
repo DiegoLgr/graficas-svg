15922287da9865b30addcb0b1ccfcef9e4548b89
Diego <diego.lgrip@gmail.com>
2020-12-23 15:07:29 +0100

    modelo de datos

--- svg/src/app/barchart/barchart.component.ts
+++ svg/src/app/barchart/barchart.component.ts
@@ -8,22 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class BarchartComponent implements OnInit {
 
     height = 500;
-    width = 300;
+    width = 500;
+    axisw = 3;
+    margl = 140;
+    margb = 75;
+    chart;
+    verticalAxis;
+    horizontalAxis;
 
     viewBox;
 
-    data = [260, 300, 10, 489, 63];
+    data = [160, 200, 10, 289, 63];
     bars;
 
     constructor() {
         this.viewBox = ` 0 0 ${this.width} ${this.height}`;
 
+        this.chart = {
+            tlx: this.margl,
+            tly: 75,
+            trx: this.width - this.margl,
+            try: 75,
+            blx: this.margl,
+            bly: this.height - this.margb,
+            brx: this.width - this.margl,
+            bry: this.height - this.margb,
+        }
+        this.verticalAxis = {
+            x1: this.chart.tlx - this.axisw/2,
+            x2: this.chart.blx - this.axisw/2,
+            y1: this.chart.tly + this.axisw,
+            y2: this.chart.bly + this.axisw,
+        }
+        this.horizontalAxis = {
+            x1: this.chart.blx - this.axisw,
+            x2: this.chart.brx - this.axisw,
+            y1: this.chart.bly + this.axisw/2,
+            y2: this.chart.bry + this.axisw/2,
+        }
+
+        const offset = 45;
         const bw =  20;
         const bm =  5;
         this.bars = this.data.map((bar, i) => {
             return {
-                x: i * (bw + bm),
-                y: this.height - bar,
+                id: i,
+                x: this.chart.blx + offset + i * (bw + bm),
+                y: this.chart.bly - bar,
                 width: bw,
                 height: bar,
                 clases: "programming",
@@ -35,4 +66,7 @@ export class BarchartComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    log(bar){
+        console.log("una barra", bar);
+    }
 }
