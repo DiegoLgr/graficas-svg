ff7f6a9ebfeba6ca96afbc69a014e17010e94264
Diego <diego.lgrip@gmail.com>
2020-12-23 14:10:45 +0100

    Sacados los atributos al angular

--- svg/src/app/barchart/barchart.component.ts
+++ svg/src/app/barchart/barchart.component.ts
@@ -8,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class BarchartComponent implements OnInit {
 
     height = 500;
-    data = [260, 300, 10, 489, 63];
+    width = 300;
 
-    constructor() { }
+    viewBox;
+
+    data = [260, 300, 10, 489, 63];
+    bars;
+
+    constructor() {
+        this.viewBox = ` 0 0 ${this.width} ${this.height}`;
+
+        const bw =  20;
+        const bm =  5;
+        this.bars = this.data.map((bar, i) => {
+            return {
+                x: i * (bw + bm),
+                y: this.height - bar,
+                width: bw,
+                height: bar,
+                clases: "programming",
+            }
+        });
+
+    }
 
     ngOnInit(): void {
     }
